037a5247f4e6816141d708c20c5ec4b535f97338
galborabia <49067563+galborabia@users.noreply.github.com>
2020-04-16 15:40:57 +0300

    check push

--- SoccerAssociationProject/src/service/GuestController.java
+++ SoccerAssociationProject/src/service/GuestController.java
@@ -6,8 +6,7 @@ public class GuestController
 {
     Guest guest;
 
-    public void register(String name, String userName, String password)
-    {
+    public void register(String name, String userName, String password) throws Exception {
         if(name!=null && userName!=null && password!=null)
         {
             if(userName.length()>6 && password.length()>8)
