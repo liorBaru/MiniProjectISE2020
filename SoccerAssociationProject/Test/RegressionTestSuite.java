4b14f62562e82bb655c5bbb69e69d40ceee5d99f
arazi_chen <arazic@post.bgu.ac.il>
2020-04-25 14:23:08 +0300

    The tests are written in Packages

--- SoccerAssociationProject/Test/RegressionTestSuite.java
+++ SoccerAssociationProject/Test/RegressionTestSuite.java
@@ -4,6 +4,16 @@ import DB.SystemTest;
 
 import domain.Asset.BoardMember;
 import domain.Asset.BoardMemberTest;
+import domain.Asset.FanTest;
+import domain.Asset.Refree.RefreeTest;
+import domain.manageEvents.NotificationTest;
+import domain.manageLeagues.GameTest;
+import domain.manageLeagues.LeagueTest;
+import domain.manageTeams.TeamTest;
+import domain.manageUsers.AccountManagerTest;
+import domain.manageUsers.AccountTest;
+import domain.manageUsers.GuestTest;
+import domain.manageUsers.UserTest;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -16,6 +26,10 @@ import service.IFAControllerTest;
 @Categories.IncludeCategory(RegressionTests.class)
 @Suite.SuiteClasses({SystemTest.class, BoardMemberTest.class,
         BoardManagerControllerTest.class, FanControllerTest.class,
-        IFAControllerTest.class})
+        IFAControllerTest.class, BoardMemberTest.class,FanTest.class,
+        RefreeTest.class, NotificationTest.class, GameTest.class,
+        LeagueTest.class,TeamTest.class, AccountManagerTest.class,
+        AccountTest.class, GuestTest.class,UserTest.class
+})
 public class RegressionTestSuite {
 }
